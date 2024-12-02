f38d56affe97b6ca6bc67cbf791ffb9b7e8c24ea
Zcy <zcy_chao1024@163.com>
2024-12-02 13:50:41 +0800

    fixed submitting error

--- src/lib/contracts/nft/awardSui.ts
+++ src/lib/contracts/nft/awardSui.ts
@@ -3,7 +3,34 @@
 import { getFullnodeUrl, SuiClient } from '@mysten/sui/client';
 import {Transaction} from "@mysten/sui/transactions";
 import {Ed25519Keypair} from '@mysten/sui/keypairs/ed25519';
-import {acceptProblem} from "@/lib/contracts"
+import {AdminList, PackageID, PersonList, ProblemList} from "@/config/key";
+
+async function acceptProblem(user: string, pid: string) {
+    const rpcUrl = getFullnodeUrl("testnet");
+    const client = new SuiClient({ url: rpcUrl });
+    const tx = new Transaction();
+    const user_address = tx.pure.address(user);
+    const list = tx.object(PersonList);
+    const move_pid = tx.pure.u64(Number(pid));
+    const problem_list = tx.object(ProblemList);
+    const admin_list = tx.object(AdminList);
+    tx.moveCall({
+        package: PackageID,
+        module: "personal",
+        function: "accept_problem",
+        arguments: [user_address, list, move_pid, problem_list, admin_list]
+    });
+    const keypair = Ed25519Keypair.fromSecretKey(process.env.PRIVATE_KEY!);
+    const result = await client.signAndExecuteTransaction({
+        transaction: tx,
+        signer: keypair,
+        requestType: 'WaitForLocalExecution',
+        options: {
+            showEffects: true,
+        }
+    });
+    return result.effects!.status.status === "success";
+}
 
 export default async function awardSui(user: string, pid: string) {
     const rpcUrl = getFullnodeUrl("mainnet");
